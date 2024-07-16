3f07085ba457511a75bddb704363ae538ad558a4
DonutSergius <popov.serg.2003.09@gmail.com>
2024-07-16 10:31:58 +0300

    Added validation on Paid vacation

--- src/FormActions/create_vacation.php
+++ src/FormActions/create_vacation.php
@@ -4,25 +4,67 @@ header('Content-Type: application/json');
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $response = ['error' => '', 'confirm' => ''];
 
+    $vacationType = $_POST['vacation-type'] ?? null;
+    $vacationTime = $_POST['vacation-time'] ?? null;
+
+    $datetimeStart = $_POST['datetime-start'] ?? null;
+    $datetimeEnd = $_POST['datetime-end'] ?? null;
+
     $dateStart = $_POST['date-start'] ?? null;
     $dateEnd = $_POST['date-end'] ?? null;
+
     $approval = $_POST['approval'] ?? null;
     $reason = $_POST['reason'] ?? null;
 
-    if ($dateStart && $dateEnd && $approval && $reason) {
+    if ($vacationType === "paid") {
+        if ($dateStart && $dateEnd && $approval && $reason) {
+            $start = new DateTime($dateStart);
+            $end = new DateTime($dateEnd);
+            $errors = array_merge(
+                cheсkStartDate($start),
+                cheсkStartEndDate($start, $end)
+            );
 
-        $startDate = new DateTime($dateStart);
-        $currentDate = new DateTime();
-
-        $formattedStartDate = $startDate->format('Y-m-d H:i:s');
-        $formattedCurrentDate = $currentDate->format('Y-m-d H:i:s');
-
-        $response['confirm'] = 'Complete success' .   $formattedStartDate . ' //' . $formattedCurrentDate;
-    } else {
-        $response['error'] = 'Please fill all field in form.';
+            $response['error'] = implode(". \n", $errors);
+        } else {
+            $response['error'] = 'Please fill all fields';
+        }
     }
-
     echo json_encode($response);
 } else {
     echo json_encode(['error' => 'Unknow error']);
+}
+
+function cheсkStartEndDate($dateStart, $dateEnd)
+{
+    $errors = [];
+    $interval = $dateStart->diff($dateEnd);
+    $daysDifference = $interval->days;
+
+    if ($dateEnd < $dateStart) {
+        $errors[] = 'Vacation cannot end before will start';
+    }
+    if ($daysDifference > 14) {
+        $errors[] = 'Vacation cannot exist more than 14 days';
+    }
+    if ($daysDifference < 7 && in_array($dateEnd->format('N'), ['6', '7'])) {
+        $errors[] = 'Vacation cannot end on Saturday or Sunday';
+    }
+
+    return $errors;
+}
+
+function cheсkStartDate($dateStart)
+{
+    $errors = [];
+    $currentDate = new DateTime();
+
+    if ($currentDate > $dateStart) {
+        $errors[] = 'You cannot take vacation on past date';
+    }
+    if (in_array($dateStart->format('N'), ['6', '7'])) {
+        $errors[] = 'Vacation cannot start on Saturday or Sunday';
+    }
+
+    return $errors;
 }
